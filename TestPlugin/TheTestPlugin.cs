ab679f9bbc63f62b16ac7d16fec74d575e0f247c
dogdie233 <dogdie233@163.com>
2024-05-01 16:34:14 +0800

    feat: 更新测试插件

--- TestPlugin/TheTestPlugin.cs
+++ TestPlugin/TheTestPlugin.cs
@@ -1,20 +1,48 @@
 ﻿using CialloBot.Plugin;
 using CialloBot.Services;
 
+using Lagrange.Core;
+using Lagrange.Core.Common.Interface.Api;
+using Lagrange.Core.Event.EventArg;
+using Lagrange.Core.Message;
+using Lagrange.Core.Message.Entity;
+
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace TestPlugin;
 
 [Plugin("com.github.dogdie233.TestPlugin", "测试插件")]
-public class TheTestPlugin(IPluginServiceContainer serviceProvider) : IPlugin
+public class TheTestPlugin : IPlugin
 {
+    private readonly ILogger<TheTestPlugin> logger;
+    private readonly LagrangeService lagrangeService;
+
+    public TheTestPlugin(IPluginServiceContainer serviceProvider)
+    {
+        logger = serviceProvider.GetRequiredService<ILogger<TheTestPlugin>>();
+        lagrangeService = serviceProvider.GetRequiredService<LagrangeService>();
+    }
+
+
     public void Startup()
     {
-        serviceProvider.GetRequiredService<CialloService>().Print(nameof(TheTestPlugin));
+        lagrangeService.BotContext.Invoker.OnGroupMessageReceived += OnGroupMessageReceived;
     }
 
     public void Shutdown()
     {
-        serviceProvider.GetRequiredService<CialloService>().Print(nameof(TheTestPlugin) + " Bye~");
+        lagrangeService.BotContext.Invoker.OnGroupMessageReceived -= OnGroupMessageReceived;
+    }
+
+    private void OnGroupMessageReceived(BotContext bot, GroupMessageEvent @event)
+    {
+        if (@event.Chain.FirstOrDefault() is not TextEntity text || text is not { Text: "ping" })
+            return;
+
+        bot.SendMessage(MessageBuilder.Group(@event.Chain.GroupUin!.Value)
+            .Forward(@event.Chain)
+            .Text("pong")
+            .Build());
     }
 }
